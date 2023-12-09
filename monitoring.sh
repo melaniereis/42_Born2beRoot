9df7bfb902a9a8b1a2f3bd9f2f9a5587b80b2c4b
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-12-09 12:20:08 +0000

    monitoring.sh : Done

--- monitoring.sh
+++ monitoring.sh
@@ -5,17 +5,14 @@ echo "<<< Server Monitor 42 >>>"
 
 # Get system information
 arch=$(uname -a)
-
 # CPU
 cpu_cores=$(grep 'core id' /proc/cpuinfo | wc -l)
 cpu_threads=$(grep 'processor' /proc/cpuinfo | wc -l)
 cpu_load=$(top -bn1 | tail +8 | awk '{cpul += $9} END {printf("%.1f"), cpul}')
-
 # RAM
 max_ram=$(free --mega | grep Mem | awk '{print $2}')
 used_ram=$(free --mega | grep Mem | awk '{print $3}')
 perc_ram=$(printf "%.2f" $(free --mega | grep Mem | awk '{print $3/$2*100}'))
-
 # Disk
 used_disk=$(df -Bm | grep "/dev/" | grep -v "/boot/" | \
 			awk '{used += $3} END {print used}')
@@ -23,7 +20,6 @@ max_disk=$(df -Bg | grep "/dev/" | grep -v "/boot/" | \
 			awk '{total += $2} END {print total}')
 perc_disk=$(df -Bm | grep "/dev/" | grep -v "/boot/" | \
 	awk '{used += $3} {total += $2} END {printf("%d"), used/total*100}')
-
 # Print last boot
 last_boot=$(who -b | awk '{print $3 " " $4}')
 # Print LVM use
@@ -32,7 +28,6 @@ if [ "$(lsblk | grep 'lvm' | wc -l)" -gt 0 ]; then
 else
 	lvm="no"
 fi
-
 # Print number of TCP connections
 tcp=$(ss -ta | grep ESTAB | wc -l)
 # Print number of users
@@ -42,7 +37,6 @@ ip=$(hostname -I)
 MAC=$(ip link | awk '$1 == "link/ether" {print $2}')
 # Print number of commands executed with sudo
 sudo=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
-
 
 # Broadcast message to all users
 echo "	#Architecture: $arch
